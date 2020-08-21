4353911a9d3e49ced22322d3d34bce45cdd6c9e2
pneto29 <69532484+pneto29@users.noreply.github.com>
2020-08-21 14:09:36 -0300

    Update icp_p2pl.cpp

--- icp_p2pl.cpp
+++ icp_p2pl.cpp
@@ -1,57 +1,7 @@
-#include <iostream>
-using namespace std;
-
-#include <algorithm>
-#include <string> 
-#include <vector>
-using std::vector;
-#define PI 3.14159265
-
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/registration/icp.h>
-#include <pcl/registration/transformation_estimation_point_to_plane.h>
-
-#include <pcl/visualization/pcl_visualizer.h>
-#include <pcl/visualization/common/common.h>
-#include <boost/thread/thread.hpp>
-
-#include <pcl/console/time.h> 
-#include <pcl/common/transforms.h>
-#include <pcl/common/centroid.h>
-#include <iostream>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/visualization/pcl_visualizer.h>
-#include <pcl/visualization/common/common.h>
-#include <boost/thread/thread.hpp>
-
-#include <pcl/console/time.h>
-#include <pcl/common/transforms.h>
-#include <pcl/common/centroid.h>
-
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/common/common.h>
-#include <pcl/registration/gicp.h>
-
-#include <boost/make_shared.hpp>
-#include <pcl/point_types.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_representation.h>
-
-#include <pcl/io/pcd_io.h>
-
+#include "validationlib.h"
+#include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/filters/voxel_grid.h>
-#include <pcl/filters/filter.h>
-
-#include <pcl/features/normal_3d.h>
-
-#include <pcl/registration/icp.h>
-#include <pcl/registration/icp_nl.h>
-#include <pcl/registration/transforms.h>
-
-#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/filters/approximate_voxel_grid.h>
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 typedef pcl::PointXYZ PointT;
 typedef pcl::PointCloud<PointT> PointCloud;
@@ -102,14 +52,12 @@ main (int argc, char** argv)
 {
     clock_t tempo;
     tempo = clock();
-    //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
-    //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out (new pcl::PointCloud<pcl::PointXYZ>);
     PointCloud::Ptr cloud_in (new PointCloud);
     PointCloud::Ptr cloud_out (new PointCloud);
     PointCloud::Ptr cloud_icp (new PointCloud);
     PointCloud::Ptr cloud_regist (new PointCloud);
 
-    //PARAMETRO - NUVEM
+
     if (pcl::io::loadPCDFile<pcl::PointXYZ> (argv[1], *cloud_out) == -1) //* load the file
 
     {
@@ -138,7 +86,6 @@ main (int argc, char** argv)
     pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
     norm_est.setSearchMethod (tree);
     norm_est.setKSearch (30);
-    //std::cout << "k: " << setKSearch << std::endl;
 
     norm_est.setInputCloud (cloud_in);
     norm_est.compute (*points_with_normals_src);
@@ -149,9 +96,7 @@ main (int argc, char** argv)
     pcl::copyPointCloud (*cloud_out, *points_with_normals_tgt);
 
     //   IterativeClosestPoint
-
-    // Align     pcl::IterativeClosestPointNonLinear<PointNormalT, PointNormalT> icp;
-
+    // Align pcl::IterativeClosestPointNonLinear<PointNormalT, PointNormalT> icp;
     pcl::IterativeClosestPoint<PointNormalT, PointNormalT> icp;
     icp.setInputSource (points_with_normals_src);
     icp.setInputTarget (points_with_normals_tgt);
@@ -161,36 +106,53 @@ main (int argc, char** argv)
     PointCloudWithNormals::Ptr reg_result = points_with_normals_src;
     //save cloud for visualization purpose
     points_with_normals_src = reg_result;
-
     // Estimate
     icp.align (*reg_result);
 
-        PointCloud::Ptr shapePos (new PointCloud);
+   PointCloud::Ptr shapePos (new PointCloud);
    transformPointCloud(*cloud_in,*shapePos,icp.getFinalTransformation());
    rotation_matrix = icp.getFinalTransformation();
 
     double rms = computeCloudRMS(cloud_out,shapePos,std::numeric_limits<double>::max ());
     //vector <double> rms_min = min(rms);
 
-    double elem1, elem2, elem3, angle123,result;
-    elem1 = rotation_matrix(0,0);
+    // double elem1, elem2, elem3, angle123, result;
+    //Calculation of rotation after alignment    
+        double elem1 = rotation_matrix(0, 0); // r_11
+        double elem2 = rotation_matrix(1, 1); // r_22
+        double elem3 = rotation_matrix(2, 2); // r_33
+        double angle123 = (elem1 + elem2 + elem3 - 1) / 2.0; // Axis–angle representation==(sum of the main diagonal - 1)/2
+      double result = acos (angle123) * 180.0 / PI;
 
-    elem2 = rotation_matrix(1,1);
-    elem3= rotation_matrix(2,2);
-    angle123= (elem1+elem2+elem3-1)/2;
-    result = acos (angle123) * 180.0 / PI;
-
-    double trans1, trans2, trans3, result2, trans12, trans22, trans32;
-    trans1 = rotation_matrix(0,3);
-    trans2 = rotation_matrix(1,3);
-    trans3 = rotation_matrix(2,3);
-    trans12= trans1*trans1;
-    trans22= trans2*trans2;
-    trans32= trans3*trans3;
-    result2=sqrt(trans12+trans22+trans32);
-
-    std::cout << "ROTAÇÃO: " << result <<":"<< std::endl;
-    std::cout << "TRANSLAÇÃO: " << ":"<<result2 << std::endl;
+    std::cout << "ROTATION: " << result <<":"<< std::endl;
     cout<<"RMSE" << rms<<":"<< endl;
     printf("Tempo:%f",(clock() - tempo) / (double)CLOCKS_PER_SEC);
+
+
+int sizePoints = 2; // size of points
+
+
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer1 (new pcl::visualization::PCLVisualizer ("REGISTRO"));
+
+    // Adding both point clouds
+    viewer1->setBackgroundColor(255,255,255);
+
+    viewer1->addPointCloud(cloud_out, "cloud_out");
+    viewer1->addPointCloud(shapePos, "cloud_in");
+
+    //// Configuring cloud_in
+    viewer1->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR,0,0,255,"cloud_in");
+    viewer1->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, sizePoints, "cloud_in");
+    // Configuring cloud_out
+    viewer1->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR,0, 255, 0,"cloud_out");
+    viewer1->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, sizePoints, "cloud_out");
+
+ // Starting visualizer
+    // viewer_final->addCoordinateSystem (1.0, "global");
+    while(!viewer1->wasStopped())
+    {
+        viewer1->spinOnce();
+        boost::this_thread::sleep (boost::posix_time::microseconds(100000));
+    }
+    system("pause");
 }
